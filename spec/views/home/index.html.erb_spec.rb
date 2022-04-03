068dd55992614a889f03f8406dcb70bda638ed0d
trevorius <trevorius45@gmail.com>
2022-04-03 08:55:04 -0400

    test render

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "home/index.html.erb", type: :view do
+RSpec.describe "home/index", type: :view do
   # pending "add some examples to (or delete) #{__FILE__}"
+  it 'renders home index page' do
+    render
+    expect(rendered).to match /Plan du Jardin/
+  end
 end
