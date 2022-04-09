3326a6ff64b0480d4ee440cc42b7d8199c5ed0d9
trevor kithchen <trevorius45@gmail.com>
2022-04-09 10:43:47 -0400

    :test: render

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe "home/index", type: :view do
     render
     expect(rendered).to match /Plan du Jardin/
   end
+  it 'assigns @title' do
+    render
+    assert_select('h1', text: 'Plan du Jardin')
+  end
 end
